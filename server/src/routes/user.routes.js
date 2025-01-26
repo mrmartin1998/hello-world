8d2ce51b11446d528cb6af9e27b4b82a848f853f
Martin Emil Brabenec <martinemilbrabenec@gmail.com>
2025-01-26 17:08:58 +0100

    hello world

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -12,4 +12,28 @@ router.get('/', async (req, res) => {
   }
 });
 
+// Create user
+router.post('/', async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Get user by ID
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id).select('-password');
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 module.exports = router; 
