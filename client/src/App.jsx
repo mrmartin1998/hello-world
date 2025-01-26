51c2934bc7156f33dc3781bdec0351778153b34d
Martin Emil Brabenec <martinemilbrabenec@gmail.com>
2025-01-26 17:51:37 +0100

    removed unnecesary files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
 
 function App() {
   return (
